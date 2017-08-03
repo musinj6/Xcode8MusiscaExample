ffd44a0d6ff41b3c7eec0865f9b08afe7fdc40b0
Nicholas Musisca <nicholasmusisca@Nicholass-MacBook-Air.local>
2017-08-03 15:19:24 -0400

    Initial Commit

---
+++ XCodeMusiscaExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XCodeMusiscaExample
+//
+//  Created by Nicholas Musisca on 8/3/17.
+//  Copyright © 2017 Nicholas Musisca. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XCodeMusiscaExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XCodeMusiscaExample
+//
+//  Created by Nicholas Musisca on 8/3/17.
+//  Copyright © 2017 Nicholas Musisca. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
